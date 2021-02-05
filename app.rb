fe3e18c835de0c8f1929fbf18e660b2401a44e01
Loren Cipriano <loren25cipriano@gmail.com>
2021-02-05 00:35:37 -0800

    commit tree e84d6b83373e4322222a0421485e6cd0f966290d

--- app.rb
+++ app.rb
@@ -2,5 +2,13 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
 
-
+  get '/' do 
+    status 200 
+    "Hello, World!"
+  end 
 end
+
+
+# GET '/'
+#   responds with a 200 status code
+#   responds with 'Hello, World!'
